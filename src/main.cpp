e666e9dd358d0f314e91dfa0799de3688d5d539e
JuNi4 <81480623+JuNi4@users.noreply.github.com>
2023-04-14 21:13:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,32 @@
-#include "nn/neuralnetwork.h"
+// This will create a neural network with random weights. It will then make a prediction and print the result.
+
+#include "nn/neuralnetwork.h" // includes the nerual network library
 #include <iostream>
 
+// a function for getting the current microseconds
 #include <chrono>
-
 uint64_t timeSinceEpochMicrosec() {
     using namespace std::chrono;
     return duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
 }
 
 int main() {
+    // gets the time at wich the programm is started, used for calculating how long the programm took to execute
+    double start = timeSinceEpochMicrosec();
+
     // setup a neural network //
-    // the sizes of the layers
+    // the sizes of the layers, the neural network will have 2 input neurons, 5 neurons on a hidden layer and 10 output neurons. 
     std::vector<int> sizes = {2,5,10};
     
-    double start = timeSinceEpochMicrosec();
-    // create the neural network
+    // create the neural network object
     nn::neuralnetwork network(sizes, &nn::sigmoid_activation);
 
-    //predict something
+    // create the input vector used for the prediction
     std::vector<std::vector<double>> inputs = { {-1,1} };
-    //network.predict( cmath::matrix( inputs ) );
+    // make a prediction based on the inputs vector and print it
     std::cout << network.predict( cmath::matrix( inputs ) ).str() << "\n";
 
-    // std::cout <<  << "\n";
-
+    // print the time it took to execute the programm
     std::cout << "end of programm, took " << timeSinceEpochMicrosec()-start << " Microseconds\n";
     return 0;
 }
