8a0a2fed2947cfe5b8b6e98f04dd0240c31fd004
JuNi4 <justus@hrtm.name>
2023-04-18 13:17:37 +0200

    Update evoloution_training.cpp

--- src/evoloution_training.cpp
+++ src/evoloution_training.cpp
@@ -7,17 +7,25 @@
 #include <nn/neuralnetwork.h>
 #include <json.hpp>
 
+#include <random>
+
 #include "dataset/mnist_dataset.hpp"
 
 using json = nlohmann::json;
 
 // Settings //
 #define SAMPLE_SIZE 10000
-#define GENERATION_SIZE 25
+#define GENERATION_SIZE 10
+#define LEARN_RATE 0.1
 
 
 // the path to the save of the network
 const char* save_path = "trained_models/number_recognition.json";
+
+uint64_t timeSinceEpochNanosec() {
+    using namespace std::chrono;
+    return duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count();
+}
 
 int highest(std::vector<double> inp) {
     // get the highest index
@@ -34,7 +42,7 @@ int highest(std::vector<double> inp) {
     return highest_i;
 }
 
-int getAccuracy( std::vector<std::vector<double>> dataset, std::vector<int> labels, nn::neuralnetwork ai ) {
+int getAccuracy( std::vector<std::vector<double>> dataset, std::vector<int> labels, nn::neuralnetwork ai, bool print_progress = true ) {
     // the amount of correct results
     int correct = 0;
     // get the sample size
@@ -44,7 +52,9 @@ int getAccuracy( std::vector<std::vector<double>> dataset, std::vector<int> labe
     #endif
     // make a lot of predictions and check if they are correct
     for (int i = 0; i < size; i++) {
-        std::cout << "[Training] Sample " << i << "/" << size << "\r";
+        if (print_progress) {
+            std::cout << "[Training] Sample " << i << "/" << size << ".\r";
+        }
         // create input matrix
         cmath::matrix input({dataset[i]});
         // make prediction
@@ -54,9 +64,69 @@ int getAccuracy( std::vector<std::vector<double>> dataset, std::vector<int> labe
             correct++;
         }
     }
-    std::cout << "[Training] Sample " << size << "/" << size << "\n";
+    if (print_progress) {
+        std::cout << "[Training] Sample " << size << "/" << size << ".\n";
+    }
     // return the amount of correct results
     return correct;
+}
+
+nn::neuralnetwork tweak(nn::neuralnetwork input) {
+    // make a lot of changes
+
+    // weights //
+    // calculate the amount of weights
+    int size = 0;
+
+    for (int wm = 0; wm < input.weights.size(); wm++) {
+        for (int wHeight = 0; wHeight < input.weights[wm].mat.size(); wHeight++) {
+            size += input.weights[wm].mat[wHeight].size();
+        }
+    }
+
+    // make weight amount of changes
+    for (int i = 0; i < size; i++) {
+        // generate wich weight to change
+        srand( timeSinceEpochNanosec() );
+        int weightMatrix = rand() % input.weights.size();
+        srand( timeSinceEpochNanosec() );
+        int matrixY = rand() % input.weights[weightMatrix].mat.size();
+        srand( timeSinceEpochNanosec() );
+        int matrixX = rand() % input.weights[weightMatrix].mat[matrixY].size();
+        // generate the change value
+        srand( timeSinceEpochNanosec() );
+        float change = ((float)(rand() % 200 + 1) / 100. - 1.) * LEARN_RATE;
+        // change the value
+        input.weights[weightMatrix].mat[matrixY][matrixX] += change;
+    }
+
+    // biases //
+    // calculate the amount of biases
+    int size = 0;
+
+    for (int bm = 0; bm < input.biases.size(); bm++) {
+        for (int bHeight = 0; bHeight < input.biases[bm].mat.size(); bHeight++) {
+            size += input.biases[bm].mat[bHeight].size();
+        }
+    }
+
+    // make weight amount of changes
+    for (int i = 0; i < size; i++) {
+        // generate wich weight to change
+        srand( timeSinceEpochNanosec() );
+        int biasMatrix = rand() % input.biases.size();
+        srand( timeSinceEpochNanosec() );
+        int matrixY = rand() % input.biases[biasMatrix].mat.size();
+        srand( timeSinceEpochNanosec() );
+        int matrixX = rand() % input.biases[biasMatrix].mat[matrixY].size();
+        // generate the change value
+        srand( timeSinceEpochNanosec() );
+        float change = ((float)(rand() % 200 + 1) / 100. - 1.) * LEARN_RATE;
+        // change the value
+        input.biases[biasMatrix].mat[matrixY][matrixX] += change;
+    }
+
+    return input;
 }
 
 int main() {
@@ -90,9 +160,75 @@ int main() {
 
     std::cout << "[Training] Initial correct answers: " << correct << "/" << size << " (" << startingPercentage*100 << "%).\n" ;
 
-    // train the network
+    // train the network //
+
+    int generationCount = 1;
+
+    // - create GENERATION_SIZE amount of networks
+    // - make minor tweaks
+    // - get performance of networks
+    // - take the best performing network and set it as the main network
+    // - repeat
+
+    // train loop
+    while (true) {
+        // a list holding all neural networks
+        std::vector<nn::neuralnetwork> networks;
+        std::vector<int> scores;
+
+        // add main network to generation
+        networks.push_back(ai);
 
 
+        std::cout << "[Training] Creating generation " << generationCount << " with a size of " << GENERATION_SIZE << ".\n";
+        // copy the main neural network x times
+        for (int i = 0; i < GENERATION_SIZE-1; i++) {
+            nn::neuralnetwork x = tweak(ai);
+            networks.push_back(x);
+        }
+
+        std::cout << "[Training] Started testing for generation "<< generationCount <<".\n";
+        // get scores
+        for (int i = 0; i < GENERATION_SIZE; i++) {
+            std::cout << "[Training] Testing network " << i+1 << "/" << GENERATION_SIZE << ".\n";
+            // get accuracy of the current network
+            int a = getAccuracy( trainingData["images"],trainingData["labels"], networks[i] );
+
+            std::cout << "[Training] Network " << i+1 << " achieved a score of " << a << ".\n";
+            // save the score
+            scores.push_back(a);
+        }
+
+        //std::cout << "[Training] Testing network " << GENERATION_SIZE << "/" << GENERATION_SIZE << ".\n";
+
+        // get the best performing network
+        int x = 0;
+        int index = 0;
+
+        std::cout << "[Training] Evaluating scores...\n";
+
+        for (int i = 0; i < GENERATION_SIZE; i++) {
+            // check if score of network is higher
+            if (scores[i] > x) {
+                // if so, set index and score
+                x = scores[i];
+                index = i;
+            }
+        }
+
+        std::cout << "[Training] Updating main network.\n";
+
+        // set main network to the best performing of the generation
+        ai = networks[index];
+
+        if (generationCount % 2 == 0) {
+            std::cout << "[Training] Saving network...\n";
+            ai.save(save_path,-1);
+        }
+
+    }
+
+    // end of programm //
     std::cout << "[Training] Reached end of programm, saving network...\n";
     // at the end, save the neural network
     ai.save(save_path, -1);
